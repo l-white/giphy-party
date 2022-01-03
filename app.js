b8d02f4370d2ed6865a294d18b1e5807b5e16d1e
Laura <law7676@outlook.com>
2022-01-03 11:56:47 -0600

    modified html and added form functionality to js file

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 console.log("Let's get this party started!");
-
-
-
-//javascript, jQuery
 const section = document.getElementById('section');
-const xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=UcA2QBIbWiL3tNbfNPwZjXRQvpQKZeFg&limit=1");
-xhr.done(function(data) { console.log("success got data", data); 
-console.log(data.data[0].url)
-//return data.data[0].url;
-const img = document.createElement('img');
-section.append(img);
-//console.log(data.url)
+
+const form = document.querySelector('form');
+form.addEventListener('submit', async function(evt){
+  evt.preventDefault();
+  let search = document.getElementById('search').value;
+  console.log(search);
+  
+  
+  const res = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: search,
+      api_key: "UcA2QBIbWiL3tNbfNPwZjXRQvpQKZeFg"
+    }
+  })
+  let image = document.createElement('img');
+  image.src = res.data.data[0].url;
+  section.append(image);
+  search.value = '';
 });
+form.reset();
+
+const button = document.getElementById('remove');
 
 
 
